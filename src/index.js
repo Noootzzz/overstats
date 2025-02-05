961ec074b15ca50578d24a0ff1df27f6ab3ebaa8
Nathan Bo <nathan.github22@gmail.com>
2025-02-05 12:11:49 +0100

    Init files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <App />,
+        // errorElement: <ErrorPage />,
+    },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
