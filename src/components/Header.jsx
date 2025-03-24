f6a82a92319db9a3627c9f8c5b627a667ad2aba6
Nathan Bo <nathan.github22@gmail.com>
2025-03-24 11:31:08 +0100

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header className='mb-5 mt-5 bg-none z-30'>
+        <header className='mb-5 mt-5 bg-none relative z-30'>
             <nav className='flex justify-center items-center space-x-10'>
                 <Link to="/heroes">Heros</Link>
                 <Link to="/"><img className='w-20' src="./img/bw.png" alt="BlackWatch Logo" /></Link>
